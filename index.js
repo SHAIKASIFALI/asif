70024ee61d85a7c4556611bf44345741f096f8bc
Sajidsk7952 <12006051@svuceedu.onmicrosoft.com>
2022-12-17 19:55:20 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+//this does tell owr command line that it has to use node to run the script
+
+console.log(` heyaa hello i am asif .. i love building clis using nodejs...
+
+I am a lazy guyyyy.... 
+well well I may be an Engineer by Profession but but a Teacher by Passion....
+If u wanna know me ...jus analyse my typing pattern......
+
+love@dot(.) don't know why ...😍😍😍
+
+`);
